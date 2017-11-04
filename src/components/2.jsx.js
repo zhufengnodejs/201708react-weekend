2e7c73b2bdc53c034f357c7cee2c2fb123b9c894
zhufengnodejs <zhufengnodejs@126.com>
2017-11-04 15:51:36 +0800

    react原理

--- src/components/2.jsx.js
+++ src/components/2.jsx.js
@@ -2,7 +2,7 @@
 import React from './react';
 import ReactDOM from './react-dom';
 // <h1 id="title">hello</h1>
-let element = React.createElement('h1',{id:'title'},'hello');
+let element = React.createElement('h1',{id:'title'},React.createElement('h2',{id:'welcome'},'world'));
 //把elementJS对象变成真实的DOM对象然后插入到页面中
 /*{
   type:'h1',
