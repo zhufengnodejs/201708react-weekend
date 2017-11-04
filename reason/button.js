b8513909e323c81722fe049e6d97fda373fcf0a5
zhufengnodejs <zhufengnodejs@126.com>
2017-11-04 10:19:57 +0800

    react原理

--- reason/button.js
+++ reason/button.js
@@ -1,11 +1,20 @@
 class LikeButton{
+  createDOMFromString(domStr){
+    let div = document.createElement('div');
+    div.innerHTML = domStr;
+    //返回这个DIV的第一个子元素
+    return div.firstElementChild;
+  }
   render(){
-    return (
-      `
+    this.element = this.createDOMFromString(`
       <button class="like-btn">
          <span class="like-text">点赞</span>
       </button>
-      `
-    )
+      `);
+    //给这个button的DOM对象增加点击事件
+    this.element.addEventListener('click',()=>{
+      alert(1);
+    });
+    return this.element;
   }
 }
