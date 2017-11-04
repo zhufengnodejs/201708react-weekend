cfa483d42353b8dbb75abd7bddd2429a78e12ab1
zhufengnodejs <zhufengnodejs@126.com>
2017-11-04 10:33:47 +0800

    react原理

--- reason/button.js
+++ reason/button.js
@@ -1,9 +1,22 @@
 class LikeButton{
+  constructor(){
+    //在构造函数中初始化一个状态对象，这是一个私有变量
+    this.state = {isLiked:false};
+    //把事件处理器中的this绑定为当前组件的实例
+    this.handleClick = this.handleClick.bind(this);
+  }
   createDOMFromString(domStr){
     let div = document.createElement('div');
     div.innerHTML = domStr;
     //返回这个DIV的第一个子元素
     return div.firstElementChild;
+  }
+  //处理点击事件
+  handleClick(){
+    console.log(this);
+    this.state.isLiked = !this.state.isLiked;
+    let likeText = document.querySelector('.like-text');
+    likeText.innerHTML = this.state.isLiked?'取消':'点赞';
   }
   render(){
     this.element = this.createDOMFromString(`
@@ -12,9 +25,7 @@ class LikeButton{
       </button>
       `);
     //给这个button的DOM对象增加点击事件
-    this.element.addEventListener('click',()=>{
-      alert(1);
-    });
+    this.element.addEventListener('click',this.handleClick);
     return this.element;
   }
 }
