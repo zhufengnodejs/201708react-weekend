7ed5d0c22ece5b854f80de35fc61c3de0c9dd5c4
zhufengnodejs <zhufengnodejs@126.com>
2017-11-04 12:47:32 +0800

    react原理

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 let path = require('path');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   //入口文件
   entry:'./src/index.js',
@@ -27,6 +28,13 @@ module.exports = {
         use:'url-loader'
       }
     ]
-  }
-  
+  },
+  plugins: [
+    //自动产出html的插件，先把js文件打包成一个bundle.js文件，然后插入index.html脚本，然后把脚本保存在build里
+    new HtmlWebpackPlugin({
+      template:'./index.html'
+    })
+  ],
+  //开发工具，实现打包后文件和源文件的映射,在报错的时候不是报bundle.js的行号，而是报源文件的文件名和行号，方便排查错误,还可以在源文件里打断点，进行调试
+  devtool:'cheap-module-source-map'
 }
