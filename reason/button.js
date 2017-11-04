0b49958744d73b023ce820977484103af9fc7596
zhufengnodejs <zhufengnodejs@126.com>
2017-11-04 10:51:55 +0800

    react原理

--- reason/button.js
+++ reason/button.js
@@ -1,9 +1,11 @@
+//希望只关注数据，当数据改变的时候，视图也会跟着改变
 class LikeButton{
   constructor(){
     //在构造函数中初始化一个状态对象，这是一个私有变量
     this.state = {isLiked:false};
     //把事件处理器中的this绑定为当前组件的实例
     this.handleClick = this.handleClick.bind(this);
+    this.setState = this.setState.bind(this);
   }
   createDOMFromString(domStr){
     let div = document.createElement('div');
@@ -11,9 +13,16 @@ class LikeButton{
     //返回这个DIV的第一个子元素
     return div.firstElementChild;
   }
+  //修改状态 新的状态对象
+  setState(newState){
+    //状态管理有一个规则 每次都要返回不同的对象,不变性
+    //this.state=Object.assign(this.state,newState);
+    this.state = Object.assign({},this.state,newState);
+    //使用新的状态对象重新渲染元素
+    this.render();
+  }
   //处理点击事件
   handleClick(){
-    console.log(this);
     this.state.isLiked = !this.state.isLiked;
     let likeText = document.querySelector('.like-text');
     likeText.innerHTML = this.state.isLiked?'取消':'点赞';
@@ -21,7 +30,7 @@ class LikeButton{
   render(){
     this.element = this.createDOMFromString(`
       <button class="like-btn">
-         <span class="like-text">点赞</span>
+         <span class="like-text">${this.state.isLiked?'取消':'点赞'}</span>
       </button>
       `);
     //给这个button的DOM对象增加点击事件
