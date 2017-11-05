5422547ff3262a502e8368944508ed1383436e55
zhufengnodejs <zhufengnodejs@126.com>
2017-11-05 17:54:03 +0800

    react原理

--- app/components/Slider.js
+++ app/components/Slider.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './Slider.less'
 import SliderItems from "./SliderItems";
 import SliderArrows from "./SliderArrows";
+import SliderDots from "./SliderDots";
 //默认导出一个Slider
 export default class Slider extends Component {
   constructor(){
@@ -13,6 +14,9 @@ export default class Slider extends Component {
     index+=step;//让老的值加上步长
     if(index>=this.props.images.length){
         index = 0;
+    }
+    if(index<0){
+      index = this.props.images.length-1;
     }
     this.setState({index});//修改状态中的index值为新的index值
   }
@@ -36,6 +40,7 @@ export default class Slider extends Component {
           index={this.state.index}
         />
         <SliderArrows turn={this.turn}/>
+        <SliderDots images={this.props.images}/>
       </div>
     )
   }
