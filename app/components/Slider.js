4d5a0a95c8e7e5afcc0bef958962fed97a6adc2c
zhufengnodejs <zhufengnodejs@126.com>
2017-11-05 15:44:53 +0800

    react原理

--- app/components/Slider.js
+++ app/components/Slider.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+//默认导出一个Slider
 export default class Slider extends Component{
     render(){
         return (
--- app/index.js
+++ app/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Slider from "./components/Slider";
+//先声明一个图片的数组
 let IMAGES = [
+  //在源代码加载图片需要require
   require('./images/1.jpg'),
   require('./images/2.jpg'),
   require('./images/3.jpg')
 ]
+//把Slider组件渲染到容器的内部
 ReactDOM.render(<Slider/>,document.querySelector('#root'));
