c70d3fea5ca0610b1968f99d07e662dc79e6f7b7
zhufengnodejs <zhufengnodejs@126.com>
2017-11-05 16:33:01 +0800

    react原理

--- app/components/Slider.js
+++ app/components/Slider.js
@@ -2,10 +2,27 @@ import React, {Component} from 'react';
 import './Slider.less'
 //默认导出一个Slider
 export default class Slider extends Component {
+  constructor(){
+    super();
+    this.state = {index:0};
+  }
+  go = ()=>{
+    setInterval(()=>{
+      this.setState({index:this.state.index+1});
+    },this.props.delay*1000)
+  }
+  componentDidMount(){
+    this.go();
+  }
   render() {
+    let style = {
+      left:this.state.index*-300,//left值，距容器左端的距离
+      width:this.props.images.length*300,//ul的实际宽度
+      transitionDuration:this.props.speed+'s' //每次动画的持续时间
+    }
     return (
       <div className="slider-wrapper">
-        <ul className="sliders">
+        <ul className="sliders" style={style}>
           {
             this.props.images.map((item,index)=>(
               <li key={index} className="slider">
