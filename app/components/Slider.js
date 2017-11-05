06aeeee2201134d51fc709ff72af42f090c05058
zhufengnodejs <zhufengnodejs@126.com>
2017-11-05 16:08:03 +0800

    react原理

--- app/components/Slider.js
+++ app/components/Slider.js
@@ -1,11 +1,19 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 //默认导出一个Slider
-export default class Slider extends Component{
-    render(){
-        return (
-            <div>
-              Slider
-            </div>
-        )
-    }
+export default class Slider extends Component {
+  render() {
+    return (
+      <div className="slider-wrapper">
+        <ul>
+          {
+            this.props.images.map((item,index)=>(
+              <li key={index}>
+                <img src={item}/>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
 }
--- app/index.js
+++ app/index.js
@@ -9,4 +9,19 @@ let IMAGES = [
   require('./images/3.jpg')
 ]
 //把Slider组件渲染到容器的内部
-ReactDOM.render(<Slider/>,document.querySelector('#root'));
+/**
+ * auto 开启自动轮播
+ * speed 每次切换图片动画持续的时间
+ * delay 每次轮播的时间间隔
+ * arrow 是否显示箭头导航
+ * dots 是否显示点状导航
+ * pause 是否当鼠标移动到轮播图上暂停轮播 移开之后继续轮播
+ */
+ReactDOM.render(<Slider
+  auto={true}
+  speed={1}
+  delay={2}
+  arrow={true}
+  dots={true}
+  pause={true}
+  images = {IMAGES}/>,document.querySelector('#root'));
